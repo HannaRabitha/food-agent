fb579ea8fdc96eb069857efc5e1caf4e5cb2cf3a
hannarabitha <h.rabitha@gmail.com>
2021-01-03 04:54:08 +0700

    add intent

--- routes/food-idn.js
+++ routes/food-idn.js
@@ -32,6 +32,7 @@ router.post('/', function(req, res, next) {
   intentMap.set('getSteps', getSteps);
   intentMap.set('getRecipe', getRecipe);
   intentMap.set('getStuff', getStuff);
+  intentMap.set('checkStuff', checkStuff);
   agent.handleRequest(intentMap);
 
 });
@@ -278,12 +279,14 @@ function getIngredients (agent) {
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX food: <http://localhost:3030/hanna/food-idn#>
        
-        SELECT DISTINCT ?foodname ?ing 
+        SELECT DISTINCT ?food_name ?ing ?step ?like ?url
         WHERE {
-          ?no rdfs:label ?foodname;
-              food:hasIng ?ing;
-              food:hasLike ?like.
-            FILTER regex(?foodname, "${foodName}", "i")
+          ?no rdfs:label ?food_name;
+          food:hasIng ?ing;
+          food:hasStep ?step;
+          food:hasURL ?url;
+          food:hasLike ?like.
+            FILTER regex(?food_name, "${foodName}", "i")
           FILTER (!regex(?ing, "${ing_stuff}","i")) 
         }
         ORDER BY DESC(?like)
@@ -295,19 +298,82 @@ function getIngredients (agent) {
             .then(aRes => {
               console.log('data ',aRes.data.results.bindings[0])
               var data = aRes.data.results.bindings[0];
-              
+
               var food = data.food_name.value;
               var ingredients= data.ing.value;
+              var step = data.step.value;
+              var link= data.url.value;
+              var like= data.like.value;
             
               var ing = ingredients.replace(/--/g , "\n");
+              var steps = step.replace(/--/g , "\n");
 
-              agent.add(food);
-              agent.add("Bahan : " +ing);
+              agent.add('Resep ' + food + '  tanpa ' + ing_stuff);
+              agent.add("Bahan-bahan : \n" +ing);
+              agent.add("Cara Membuat : \n" +steps);
+              agent.add("link : " +link);
+              agent.add(+like + "  orang menyukai ini");
          
           }).catch (error => {
             console.log("Something is wrong  !! ");
             console.log(error);
             agent.add('Makanan tidak ditemukan');
+        });
+      };
+
+
+      function checkStuff (agent) {
+        var foodName = agent.parameters["foodName"];
+        var ing_stuff = agent.parameters["stuff"];
+  
+        console.info(foodName);
+        console.info(ing_stuff);
+        
+        var endpoint = 'http://localhost:3030/food-idn/sparql';
+        var query =  `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX food: <http://localhost:3030/hanna/food-idn#>
+       
+        SELECT DISTINCT ?food_name ?ing ?step ?like ?url
+        WHERE {
+          ?no rdfs:label ?food_name;
+          food:hasIng ?ing;
+          food:hasStep ?step;
+          food:hasURL ?url;
+          food:hasLike ?like.
+            FILTER regex(?food_name, "${foodName}", "i")
+          FILTER regex(?ing, "${ing_stuff}","i")
+        }
+        ORDER BY DESC(?like)
+        `;
+      
+        var queryUrl = endpoint + "?query=" + encodeURIComponent(query) + "&format=json";
+      
+          return getData(queryUrl)
+            .then(aRes => {
+              console.log('data ',aRes.data.results.bindings[0])
+              var data = aRes.data.results.bindings[0];
+
+              var food = data.food_name.value;
+              var ingredients= data.ing.value;
+              var step = data.step.value;
+              var link= data.url.value;
+              var like= data.like.value;
+            
+              var ing = ingredients.replace(/--/g , "\n");
+              var steps = step.replace(/--/g , "\n");
+
+            //   agent.add('Resep ' + food + '  tanpa ' + ing_stuff);
+            //   agent.add("Bahan-bahan : \n" +ing);
+            //   agent.add("Cara Membuat : \n" +steps);
+            //   agent.add("link : " +link);
+            //   agent.add(+like + "  orang menyukai ini");
+            agent.add('Iya, ' +foodName+ ' menggunakan ' +ing_stuff)
+         
+          }).catch (error => {
+            console.log("Something is wrong  !! ");
+            console.log(error);
+            agent.add('Tidak, ' +foodName+ ' tidak menggunakan ' +ing_stuff)
         });
       };
 
