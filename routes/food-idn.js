863305c7f08586c593a9d9623f8bd0085eb65ea3
hannarabitha <h.rabitha@gmail.com>
2021-01-02 20:56:12 +0700

    commit on main

--- routes/food-idn.js
+++ routes/food-idn.js
@@ -4,12 +4,9 @@ var router = express.Router();
 const dfff = require('dialogflow-fulfillment');
 const bodyParser = require('body-parser');
 
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-
 
 router.get('/', function(req, res, next){
     res.send("Hello World, this food-idn.js")
@@ -18,7 +15,6 @@ router.get('/', function(req, res, next){
 
 
 router.post('/', function(req, res, next) {
-
   const agent = new dfff.WebhookClient({
     request : req,
     response : res
@@ -30,11 +26,15 @@ router.post('/', function(req, res, next) {
 
   let intentMap = new Map();
   intentMap.set('webhookTest', Test);
+  intentMap.set('getFood', getFood);
   intentMap.set('getPopulerFood', getPopulerFood);
-//   intentMap.set('getFood', GetFood);
+  intentMap.set('getIngredients', getIngredients);
+  intentMap.set('getSteps', getSteps);
   agent.handleRequest(intentMap);
 
 });
+
+
 
 
  
@@ -43,9 +43,14 @@ function Test (agent) {
 }
 
 
-function getPopulerFood (agent) {
-//   var product = agent.parameters["product"];
-  
+function getData(queryUrl) {
+    const axios = require('axios');
+    return axios
+        .get(queryUrl);
+  }
+
+
+  function getPopulerFood (agent) {
   var endpoint = 'http://localhost:3030/food-idn/sparql';
   var query =  `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -64,7 +69,7 @@ function getPopulerFood (agent) {
   var foodArray = [];
   var numberArray= [];
 
-    return getFood(queryUrl)
+    return getData(queryUrl)
       .then(aRes => {
         console.log('data ',aRes.data.results.bindings)
         var data = aRes.data.results.bindings;
@@ -76,23 +81,98 @@ function getPopulerFood (agent) {
               agent.add('- ' + foodArray[i]);
         }
 
-    // let bot_response = `10 Makanan Paling Populer : ${foodArray}`;
-
-    // console.log(bot_response);
-    // agent.add(bot_response);
     }).catch (error => {
       console.log("Something is wrong  !! ");
       console.log(error);
-    //   agent.add(bot_response);
   });
 };
 
+function getFood (agent) {
+      var foodName = agent.parameters["foodLike"];
+      
+      var endpoint = 'http://localhost:3030/food-idn/sparql';
+      var query =  `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX food: <http://localhost:3030/hanna/food-idn#>
+      SELECT DISTINCT ?food_name ?like
+      WHERE {
+        ?no rdfs:label ?food_name;
+              food:hasLike ?like;
+              FILTER regex(?food_name, "${foodName}", "i")
+      }
+      ORDER BY DESC(?like)
+      LIMIT 10
+      `;
+    
+      var queryUrl = endpoint + "?query=" + encodeURIComponent(query) + "&format=json";
+    
+      var foodArray = [];
+      var numberArray= [];
+    
+        return getData(queryUrl)
+          .then(aRes => {
+            console.log('data ',aRes.data.results.bindings)
+            var data = aRes.data.results.bindings;
+            
+            agent.add(`Rekomendasi Masakan  ${foodName} : `);
+            for(var i in data) {    
+              var item = data[i];  
+                  foodArray[i] = item.food_name.value;
+                  numberArray[i] = item.like.value;
+                  agent.add('- ' + foodArray[i]);
+            }
+    
+        }).catch (error => {
+          console.log("Something is wrong  !! ");
+          console.log(error);
+          agent.add("Data tidak ditemukan");
+      });
+    };
 
-function getFood(queryUrl) {
-  const axios = require('axios');
-  return axios
-      .get(queryUrl);
-}
+
+function getIngredients (agent) {
+      var foodName = agent.parameters["foodName"];
+
+      console.info(foodName);
+      
+      var endpoint = 'http://localhost:3030/food-idn/sparql';
+      var query =  `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX food: <http://localhost:3030/hanna/food-idn#>
+      SELECT DISTINCT ?food_name ?ing ?like ?url
+      WHERE {
+        ?no rdfs:label ?food_name;
+            food:hasIng ?ing;
+            food:hasURL ?url;
+            food:hasLike ?like.
+        FILTER regex(?food_name, "${foodName}", "i")
+      }
+      ORDER BY DESC(?like)
+      `;
+    
+      var queryUrl = endpoint + "?query=" + encodeURIComponent(query) + "&format=json";
+    
+        return getData(queryUrl)
+          .then(aRes => {
+            console.log('data ',aRes.data.results.bindings[0])
+            var data = aRes.data.results.bindings[0];
+            var food = data.food_name.value;
+            var ingredients= data.ing.value;
+            var link= data.url.value;
+            var ing = ingredients.replace(/--/g , "\n");
+            agent.add('Bahan Makanan ' + food);
+            agent.add(ing);
+            agent.add("link : " +link);
+       
+        }).catch (error => {
+          console.log("Something is wrong  !! ");
+          console.log(error);
+          agent.add('Makanan tidak ditemukan');
+      });
+    };
+
+
+
 
 
 
