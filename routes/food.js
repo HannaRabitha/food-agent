658d09ab77fbc7d7b4f51f9e79b7610296f6104e
hannarabitha <h.rabitha@gmail.com>
2021-01-05 12:10:27 +0700

    Intent searchFood

--- routes/food.js
+++ routes/food.js
@@ -5,6 +5,7 @@ var router = express.Router();
 const dfff = require('dialogflow-fulfillment');
 const bodyParser = require('body-parser');
 const translate = require('@k3rn31p4nic/google-translate-api');
+// const translate = require('translate-google');
 
 const app = express();
 app.use(bodyParser.json());
@@ -28,7 +29,7 @@ router.post('/', function(req, res, next) {
 
   let intentMap = new Map();
   intentMap.set('webhookTest', webhookTest);
-  // intentMap.set('getFood', GetFood);
+  intentMap.set('searchFood', searchFood);
   agent.handleRequest(intentMap);
 
 });
@@ -39,48 +40,99 @@ function webhookTest(agent) {
   agent.add("sending response from webhook server")
 }
 
+function trans(foodName_id) {
+  translate(foodName_id, {from:'id', to: 'en' }).then(res => {
+    console.info(res); // OUTPUT: You are amazing!
+    return(res);
+  }).catch(err => {
+    console.error(err);
+  });
+}
 
-// function GetFood (agent) {
-//   var product = agent.parameters["product"];
+function searchFood (agent) {
+  var foodName_id = agent.parameters["foodName"];
+  var foodName_en;
+  console.info(foodName_id);
+
+  return translate(foodName_id, {from: 'id', to: 'en'}).then(
+    res=> {
+      console.log(res.text);
+      foodName_en=res.text;
+  // foodName_en =trans(foodName_id);
+
+  console.log("makanan untuk " +foodName_en);
   
-//   var endpoint = 'http://localhost:3030/food-hanna/sparql';
-//   var query =  `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-//   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-//   PREFIX food: <http://www.paguyubankseusu.org/hanna/hanna-food.owl#>
-//   SELECT ?food_name 
-//   WHERE {
-//     ?no food:has_name ?food_name;
+  var endpoint = 'http://localhost:3030/food-BFPD/sparql';
+  var query =  
+  `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX food: <http://localhost:3030/hanna/food-BFPD#>
+          SELECT DISTINCT ?food_name ?man ?ing ?nut ?ss ?sh
+          WHERE {
+           ?a food:hasName ?food_name;
+              food:productBy ?man;
+              food:hasIng ?ing;
+          food:hasNutrient ?nut;
+           food:hasServingSize ?ss;
+             food:hasServingHousehold ?sh
+              FILTER regex(?food_name, "${foodName_en}", "i")
+          }
+      order by strlen(str(?food_name))
+      LIMIT 7`;
+
+  var queryUrl = endpoint + "?query=" + encodeURIComponent(query) + "&format=json";
+
+  var foodArray = [];
+  // var foodArray_en = [];
+
+  return getURL(queryUrl)
+      .then(aRes => {
+        console.log('data ',aRes.data.results.bindings)
+        var data = aRes.data.results.bindings;
         
-//     FILTER regex(?food_name, "${product}", 'i')
-//   }`;
+        agent.add(`Produk makanan ${foodName_id} : `);
+        for(var i in data) {    
+          var item = data[i];  
+              foodArray[i] = item.food_name.value;
+              // return translate(foodArray[i], {from:'en', to: 'id' }).then(ress => {
+              //   console.info(ress.text); // OUTPUT: You are amazing!
+              //   foodArray_en[i]=ress.text;
+              //   agent.add('- ' + foodArray_en[i]);
 
-//   var queryUrl = endpoint + "?query=" + encodeURIComponent(query) + "&format=json";
-
-//   var foodArray = [];
-
-//     return getFood(queryUrl)
-//       .then(aRes => {
-//         console.log('data ',aRes.data.results.bindings)
-//         var data = aRes.data.results.bindings;
-        
-//         for(var i in data) {    
-//           var item = data[i];  
-//               foodArray[i] = item.food_name.value;
-//         }
-
-//     let bot_response = `Food is : ${foodArray}`;
-
-//     console.log(bot_response);
-//     agent.add(bot_response);
-//     }).catch (error => {
-//       console.log("Something is wrong  !! ");
-//       console.log(error);
-//       agent.add(bot_response);
-//   });
-// };
+              // })
+              // .catch(err => {
+              //   console.error(err);
+              // });
+              agent.add('- ' + foodArray[i]);
+        }
+          
+          // var foodArraystr= foodArray.toString();
+          // return translate(foodArraystr, {from:'en', to: 'id' }).then(ress => {
+          //       console.info(ress.text); // OUTPUT: You are amazing!
+          //       var foodArray_en=ress.text;
+          //       agent.add(foodArray_en);
+          //     })
+          //     .catch(err => {
+          //       console.error(err);
+          //     });
 
 
-function getFood(queryUrl) {
+    }).catch (error => {
+      console.log("Something is wrong  !! ");
+      console.log(error);
+      var bot_response ="Data tidak ditemukan";
+      agent.add(bot_response);
+  });
+
+
+}
+)
+
+
+};
+
+
+function getURL(queryUrl) {
   const axios = require('axios');
   return axios
       .get(queryUrl);
